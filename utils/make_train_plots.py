cdeec88e3af390c1bae78cca6fb3a0765c58e567
sashank tirumala <sashank.tirumala@nymble.in>
2019-05-22 17:06:17 +0530

    Added a small plot function, to plot returns versus steps

--- utils/make_train_plots.py
+++ utils/make_train_plots.py
@@ -35,3 +35,28 @@ def make_train_plots(log = None,
             plt.savefig(save_loc+'/'+key+'.png', dpi=100)
             plt.close()
 
+
+def make_train_plots_ars(log = None,
+                     log_path = None,
+                     keys = None,
+                     save_loc = None):
+    """
+    Plots and saves images of all logged data
+    :param log : A dictionary containing lists of data we want
+    :param log_path : Path to a csv file that contains all the logged data
+    :param keys : The keys of the dictionary, for the data you want to plot
+    :param save_loc : Location where you want to save the images
+    :returns : Nothing, saves the figures of the plot
+    """
+    if log is None and log_path is None:
+        print("Need to provide either the log or path to a log file")
+    if log is None:
+        logger = DataLog()
+        logger.read_log(log_path)
+        log = logger.log
+
+    plt.figure(figsize=(10,6))
+    plt.plot(log[keys[0]], log[keys[1]])
+    plt.title(keys[1])
+    plt.savefig(save_loc+'/'+keys[1]+'.png', dpi=100)
+    plt.close()
