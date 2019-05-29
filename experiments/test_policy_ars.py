9680d476388c10469b2daf8f563f4ec16c4f64d5
Abhik Singla <bhsapiens91@gmail.com>
2019-05-29 09:25:53 +0530

    Some of the good results from ARS on Stoch2 Robot is what I'm commiting

--- experiments/test_policy_ars.py
+++ experiments/test_policy_ars.py
@@ -9,11 +9,14 @@ import pybRL.policies.gaussian_linear as gaussian_linear
 import pybullet_envs
 import pybullet_envs.bullet.minitaur_gym_env as e
 import pybullet_envs.minitaur.envs.minitaur_trotting_env as e
+import pybRL.envs.stoch2_gym_bullet_env as e
 import pybullet as p
 import numpy as np
 # p.connect(p.GUI)
-env = e.MinitaurTrottingEnv(render=True)
+#env = e.MinitaurTrottingEnv(render=True)
+env = e.Stoch2Env(render = True)
 path = '/home/sashank/mjrl-master/pybRL/experiments/policy_MinitaurTrottingEnv-v0_20190522-110536.npy'
+path = '/home/abhik/pybRL/experiments/Stoch2_ARS_1/iterations/best_policy.npy'
 state = env.reset()
 
 i = 0
