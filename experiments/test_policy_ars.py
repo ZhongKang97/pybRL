2beef8e9410d5f80634cc84fa164f5475c778eee
sashank-tirumala <stsashank6@gmail.com>
2019-06-10 10:15:21 +0530

    Added normalization to test_policy_ars, so that it matches the training

--- experiments/test_policy_ars.py
+++ experiments/test_policy_ars.py
@@ -15,20 +15,45 @@ import pybRL.envs.stoch2_gym_bullet_env_normal as e
 import pybullet as p
 import numpy as np
 import time
+
+class Normalizer():
+
+  def __init__(self, nb_inputs):
+    self.n = np.zeros(nb_inputs)
+    self.mean = np.zeros(nb_inputs)
+    self.mean_diff = np.zeros(nb_inputs)
+    self.var = np.zeros(nb_inputs)
+
+  def observe(self, x):
+    self.n += 1.
+    last_mean = self.mean.copy()
+    self.mean += (x - self.mean) / self.n
+    self.mean_diff += (x - last_mean) * (x - self.mean)
+    self.var = (self.mean_diff / self.n).clip(min=1e-2)
+
+  def normalize(self, inputs):
+    obs_mean = self.mean
+    obs_std = np.sqrt(self.var)
+    return (inputs - obs_mean) / obs_std
+
 # p.connect(p.GUI)
 # env = e.MinitaurTrottingEnv(render=True)
 env = e.StochBulletEnv(render = True, gait = 'trot', energy_weight = 10.0 )
 # path = '/home/sashank/mjrl-master/pybRL/experiments/policy_MinitaurTrottingEnv-v0_20190522-110536.npy'
 # path = '/home/abhik/pybRL/experiments/Stoch2_ARS_1/iterations/best_policy.npy'
-path = os.path.realpath('../..') + '/pybRL/experiments/Stoch2_wt_2/iterations/policy_0.npy'
+path = os.path.realpath('../..') + '/pybRL/experiments/Stoch_Test/iterations/policy_0.npy'
 state = env.reset()
+nb_inputs = env.observation_space.sample().shape[0]
+normalizer = Normalizer(nb_inputs)
 logger = DataLog()
 i = 0
 policy = np.load(path)
 print(policy)
 total_reward = 0
 while i<1000:
-    action = np.matmul(policy, state)
+    normalizer.observe(state)
+    state = normalizer.normalize(state)
+    action = np.clip(np.matmul(policy, state), -1, 1)
     state, reward, done, info = env.step(action)
     total_reward = total_reward + reward
     i =i+1
