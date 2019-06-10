Made normalizationof states start from scratch for each episode during training. Fixed the standard deviation, it is now only calculated for the best directions and not for all directions

experiments/ars_multi.py
@@ -165,7 +165,9 @@ class Policy():
 # Exploring the policy on one specific direction and over one episode
 
 
-def explore(env, normalizer, policy, direction, delta, hp):
+def explore(env, policy, direction, delta, hp):
+  nb_inputs = env.observation_space.sample().shape[0]
+  normalizer = Normalizer(nb_inputs)
   state = env.reset()
   done = False
   num_plays = 0.
@@ -238,15 +240,12 @@ def train(env, policy, normalizer, hp, parentPipes, args):
     else:
       # Getting the positive rewards in the positive directions
       for k in range(hp.nb_directions):
-        positive_rewards[k] = explore(env, normalizer, policy, "positive", deltas[k], hp)
+        positive_rewards[k] = explore(env, policy, "positive", deltas[k], hp)
 
       # Getting the negative rewards in the negative/opposite directions
       for k in range(hp.nb_directions):
-        negative_rewards[k] = explore(env, normalizer, policy, "negative", deltas[k], hp)
+        negative_rewards[k] = explore(env, policy, "negative", deltas[k], hp)
 
-    # Gathering all the positive/negative rewards to compute the standard deviation of these rewards
-    all_rewards = np.array(positive_rewards + negative_rewards)
-    sigma_r = all_rewards.std()
     # Sorting the rollouts by the max(r_pos, r_neg) and selecting the best directions
     scores = {
         k: max(r_pos, r_neg)
@@ -254,12 +253,15 @@ def train(env, policy, normalizer, hp, parentPipes, args):
     }
     order = sorted(scores.keys(), key=lambda x: -scores[x])[:int(hp.nb_best_directions)]
     rollouts = [(positive_rewards[k], negative_rewards[k], deltas[k]) for k in order]
-
+   
+    # Gathering all the positive/negative rewards to compute the standard deviation of these rewards
+    all_rewards = np.array([x[0] for x in rollouts] + [x[1] for x in rollouts])
+    sigma_r = all_rewards.std() # Standard deviation of only rewards in the best directions is what it should be
     # Updating our policy
     policy.update(rollouts, sigma_r, args)
 
     # Printing the final reward of the policy after the update
-    reward_evaluation = explore(env, normalizer, policy, None, None, hp)
+    reward_evaluation = explore(env, policy, None, None, hp)
     logger.log_kv('steps', total_steps)
     logger.log_kv('return', reward_evaluation)
     if(reward_evaluation > best_return):

experiments/test_policy_ars.py
@@ -1,6 +1,6 @@
 import sys, os
 sys.path.append(os.path.realpath('../..'))
-sys.path.append('/home/sashank/stoch2_gym_env')
+# sys.path.append('/home/sashank/stoch2_gym_env')
 import pickle
 import pybRL.baselines.mlp_baseline as mlp_baseline
 from pybRL.utils.gym_env import GymEnv
@@ -17,18 +17,20 @@ import numpy as np
 import time
 # p.connect(p.GUI)
 # env = e.MinitaurTrottingEnv(render=True)
-env = e.StochBulletEnv(render = True, gait = 'trot')
-path = '/home/sashank/mjrl-master/pybRL/experiments/policy_MinitaurTrottingEnv-v0_20190522-110536.npy'
+env = e.StochBulletEnv(render = True, gait = 'trot', energy_weight = 10.0 )
+# path = '/home/sashank/mjrl-master/pybRL/experiments/policy_MinitaurTrottingEnv-v0_20190522-110536.npy'
 # path = '/home/abhik/pybRL/experiments/Stoch2_ARS_1/iterations/best_policy.npy'
-path = '/home/rbccps/pybRL/experiments/Stoch_Test/iterations/best_policy.npy'
+path = os.path.realpath('../..') + '/pybRL/experiments/Stoch2_wt_2/iterations/policy_0.npy'
 state = env.reset()
 logger = DataLog()
 i = 0
 policy = np.load(path)
 print(policy)
-while i<20:
+total_reward = 0
+while i<1000:
     action = np.matmul(policy, state)
-    state, reward, done, info = env.step(np.clip(action, -1, 1))
+    state, reward, done, info = env.step(action)
+    total_reward = total_reward + reward
     i =i+1
     logger.log_kv('x_leg1', info['xpos'][0])
     logger.log_kv('x_leg2', info['xpos'][1])
@@ -36,5 +38,6 @@ while i<20:
     logger.log_kv('y_leg2', info['ypos'][1])
 
     # time.sleep(1./30.)
+print(total_reward)
 
 plotter.plot_traj(logger, ['x_leg1', 'x_leg2'], ['y_leg1', 'y_leg2'], ['Leg1 Trajectory, rep:5', 'Leg2 Trajectory, rep:5'], save_loc= './')