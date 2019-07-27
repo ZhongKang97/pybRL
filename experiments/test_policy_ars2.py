e8fcf0bd0de369e8eb6dca421dcc864f0ef3b3fb
Abhik Singla <bhsapiens91@gmail.com>
2019-07-27 22:18:49 +0530

    Changed it to print the actual action, so that we can easily check it later

--- experiments/test_policy_ars2.py
+++ experiments/test_policy_ars2.py
@@ -50,12 +50,16 @@ bound = [0, 0, PI, PI]
 trot = [0, PI, PI , 0]
 custom_phase = [0, PI, PI+0.1 , 0.1]
 
-env = e2.Stoch2Env(render = True, phase = custom_phase, stairs = True)
+# env = e2.Stoch2Env(render = True, phase = custom_phase, stairs = True)
+env = e2.Stoch2Env(render = True, phase = walk, stairs = False)
+
 # env = e4.Stoch2Env(render = True)
 # path = '/home/sashank/mjrl-master/pybRL/experiments/policy_MinitaurTrottingEnv-v0_20190522-110536.npy'
 # path = '/home/abhik/pybRL/experiments/Stoch2_ARS_1/iterations/best_policy.npy'
 #'/pybRL/experiments/Stoch2_Jun14_9/iterations/policy_10.npy'
 path = '/pybRL/experiments/spline/Jul26_stairs/iterations/best_policy.npy'
+path = '/pybRL/experiments/spline/Jul27_1/iterations/best_policy.npy'
+
 path = os.path.realpath('../..') + path
 state = env.reset()
 nb_inputs = env.observation_space.sample().shape[0]
@@ -70,12 +74,30 @@ total_reward = 0
 states = []
 # action = np.array([ 0.24504616, -0.11582746,  0.71558934, -0.46091432, -0.36284493,  0.00495828,
 #  -0.06466855, -0.45247894,  0.72117291, -0.11068088])
+mul_ref =0
+action = policy.dot(state)
+
+if(action.size == 10):
+  mul_ref = np.array([0.08233419, 0.07341638, 0.04249794, 0.04249729, 0.07341638, 0.08183298,0.07368498, 0.04149645, 0.04159619, 0.07313576])
+elif(action.size == 18):
+  mul_ref = np.array([0.08733419, 0.07801237, 0.07310331, 0.05280192, 0.04580373, 0.04580335, 0.05280085, 0.07310168, 0.07801237, 0.08683298, 0.11530908, 0.07157067, 0.05135627, 0.0447909,  0.04467491, 0.05151569, 0.0710504,  0.11530908])
+elif(action.size == 20):
+  mul_ref = np.array([0.08733419, 0.07832142, 0.07841638, 0.05661231, 0.04749794, 0.045, 0.04749729, 0.05661107, 0.07841638, 0.07832142, 0.08683298, 0.1112868, 0.07868498, 0.05570797, 0.04649645, 0.04400026, 0.04659619, 0.0553098, 0.07813576, 0.1112868 ])
 while i<1000:
     action = policy.dot(state)
     # action = np.ones(18)
     action = np.clip(action, -1, 1)
-    print(action)
     state, reward, done, info = env.step(action)
+    actionf = np.multiply(action, mul_ref) * 0.5
+    action_spline_ref = np.multiply(np.ones(action.size),mul_ref) * 0.5
+    actionf = actionf + action_spline_ref
+    actionf = np.append(actionf, actionf[0])
+
+    action_str = '{'
+    for x in actionf:
+      action_str = action_str + str(x) + ','
+    action_str = action_str[:-1] + '};'
+    print(action_str)
     states.append(state)
     # env.step(env.action_space.sample())
     # print(reward)
