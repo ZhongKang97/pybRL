6ea0dcdf511298b265e5cc571385215dbb77e757
sashank tirumala <sashank.tirumala@nymble.in>
2019-08-12 14:30:50 +0530

    Made a properly working and robust data extractor from simulation

--- sim2real/sim.py
+++ sim2real/sim.py
@@ -3,36 +3,48 @@ sys.path.append(os.path.realpath('../..'))
 # sys.path.append('/home/sashank/stoch2_gym_env')
 from pybRL.utils.logger import DataLog
 import pybRL.envs.stoch2_gym_bullet_env_bezier as e2
-import pybullet as p
 import numpy as np
-import time
-from math import ceil
-def clean_data(angle_array):
-  no_of_rows = angle_array.shape[0]
-  final_data = []
-  #200 points actually repeat
-  for i in range(199):
-    current_angle = angle_array[int(i*no_of_rows/200)]
-    current_angle = np.delete(current_angle, [0])
-    current_angle = np.insert(current_angle,0,i)
-    final_data.append(current_angle)
-  current_angle = angle_array[-1]
-  current_angle = np.delete(current_angle, [0])
-  current_angle = np.insert(current_angle,0,199)
-  final_data.append(current_angle)
-  return np.array(final_data)
-env = e2.Stoch2Env(render = True)
-action1= np.array([0.10764433, -0.22723689,  0.37480827,  0.01160054, -0.18521147,  0.01536253,
-  0.07677522, -0.65195124,  0.0739685,  -0.23965774])
+import pandas as pd
+import math
+
+df = pd.DataFrame(columns = ['index','flh', 'flk', 'frh', 'frk','blh', 'blk', 'brh', 'brk'])
+
+def clean_data(info, state, no_of_pts):
+  no_of_rows = len(info) 
+  x = [int(np.around(x)) for x in np.linspace(0, no_of_rows-1, no_of_pts-1)]
+  final_info = []
+  counter = 0
+  for count in x:
+    temp_dict = {'index': counter ,'flh':info[count][1] ,'flk': info[count][2], 'frh': info[count][3], 
+    'frk': info[count][4],'blh': info[count][5], 'blk': info[count][6], 'brh': info[count][7], 'brk' : info[count][8]}
+    final_info.append(temp_dict)
+    counter= counter+1
+  final_info.append({'index': counter ,'flh':state[0] ,'flk':state[1], 'frh':state[2],'frk':state[3],'blh':state[4], 'blk':state[5], 'brh':state[6], 'brk' :state[7]})
+  return final_info
+
+PI = math.pi
+walk = [0, PI, PI/2, 3*PI/2]
+pace = [0, PI, 0, PI]
+bound = [0, 0, PI, PI]
+trot = [0, PI, PI , 0]
+custom_phase = [0, PI, PI+0.1 , 0.1]
+
+env = e2.Stoch2Env(render = False, phase = trot, stairs = False)
+action = np.array([0.4143, 0.2379, 0.0385, 0.0826, 0.0119, 0.0402, 0.031, 0.063, 0.157, 0.1575, 0.1267, 0.0596, 0.0156, 0.0019, 0.0431, 0.0986, 0.1905, 0.516])
+
 i = 0
 infos = []
+
+desired_pt_count = 300
+
 while i<5:
-    state, reward, done, info = env.step(action1)
-    if( i >=3  and i <  5):
-      infos.append(np.array(info))
+    state, reward, done, info = env.step(action)
+    print(state)
+    if(i >=3):
+      info = clean_data(info,state,desired_pt_count)
+      df = pd.concat([df, pd.DataFrame(info)])
     i =i+1
-print(infos)
-# final_info = np.concatenate((infos[0], infos[1]))
+    pass
+print(df)
 # final_info = clean_data(final_info)
 # np.savetxt("sim_action1_test.txt", final_info)
-# ]
